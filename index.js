1c954f92a066befb4ca12ef7e16c64bdff47ce7a
Inixi Noisse <inixi@idwt.pl>
2021-02-22 13:02:13 +0100

    better error handling and sdk update

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ const url = require('url');
 const https = require('https');
 const cprocess = require('child_process')
 
-const available_versions = [
-  '295.0.0',
-  '296.0.0',
-  '296.0.1',
-  '297,0.0',
-  '297.0.1',
-  '298.0.0',
-  '299.0.0',
-  '300.0.0',
-  '301.0.0',
-  '302.0.0',
-  '303.0.0',
-  '304.0.0',
-  '305.0.0',
-  '306.0.0',
-  '307.0.0',
-  '308.0.0',
-  '309.0.0',
-  '310.0.0',
-  '311.0.0',
-  '312.0.0'
-]
 const download_dir = `${__dirname}/.gcloudsdk`
 const download_file = `${download_dir}/gcloud-sdk.tar.gz`
 const gcloud_credentials_file = `${download_dir}/creds`
@@ -49,40 +27,50 @@ function create_dir(directory) {
 }
 
 function download_gcloud_sdk(version) {
-  if (available_versions.includes(version)) {
-    console.log(`Downloading GCloud SDK version ${version}`);
-    create_dir(
-      download_dir
-    )
-    download_url = `https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-linux-x86_64.tar.gz`
-    const options = {
-      host: url.parse(download_url).host,
-      port: 443,
-      path: url.parse(download_url).pathname
-    }
-    var file = fs.createWriteStream(download_file);
-    https.get(
-      options,
-      function(res) {
+  console.log(`Downloading GCloud SDK version ${version}`);
+  create_dir(
+    download_dir
+  )
+  download_url = `https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-linux-x86_64.tar.gz`
+  const options = {
+    host: url.parse(download_url).host,
+    port: 443,
+    path: url.parse(download_url).pathname
+  }
+  var file = fs.createWriteStream(download_file);
+  https.get(
+    options,
+    function(res) {
+      if (res.statusCode != 200) {
+        console.error(`File ${download_url} failed to download`);
+        console.error(`status code: ${res.statusCode}`);
+        console.error(`message: ${res.statusMessage}`);
+        process.exit(1);''
+      } else {
         res.on(
           'data',
           function(data) {
             file.write(data);
           }
         ).on(
+          'error',
+          function(error) {
+            file.end();
+            console.error(`File ${download_url} could not be downloaded, ${error}`)
+          }
+        )
+        .on(
           'end',
-          function() {
+          function(end) {
             file.end();
             console.log(`File ${download_url} downloaded successfully`);
+            console.log(end)
             unarchive_gcloud_sdk();
           }
         )
       }
-    )
-  } else {
-    console.error(`cannot find version ${version}`)
-    process.exit(1)
-  }
+    }
+  )
 }
 
 function unarchive_gcloud_sdk() {
